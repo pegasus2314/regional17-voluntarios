14115f93765237fe9ada2e884b554d6acadd0c50
pegasus2314 <itsalberthjesus@gmail.com>
2026-08-15 07:00:24 +0200

    Load admin role management and UX helpers

--- config.js
+++ config.js
@@ -1,1 +1,2 @@
 window.RV_CONFIG={SUPABASE_URL:'https://ibsmrkwkmcjyekwllxic.supabase.co',SUPABASE_ANON_KEY:'sb_publishable_9vAku5bk8kjr55NdrheYEQ_0R5daGfS',MAP_DEFAULT_LAT:18.808,MAP_DEFAULT_LNG:-69.784,MAP_DEFAULT_ZOOM:10};
+(function(){var s=document.createElement('script');s.src='admin.js';s.defer=true;document.head.appendChild(s)})();
